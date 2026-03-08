1de617636532634591a39731ecdddff406ef5049
mirih-13 <miriamhaf@outlook.de>
2026-03-08 20:44:06 +0100

    Selecting and renaming relevant variables

--- code/data_prep.R
+++ code/data_prep.R
@@ -1,3 +1,16 @@
-load_data <- function() {
-  # TODO: implement data loading
-}
+library(tidyverse)
+
+
+data_labour_force <- read.csv("data/raw/ilo_labour_force_participation.csv.gz")
+
+#Umbenennen relevanter Variablen
+data_labour_force <- rename(data_labour_force,
+       country = ref_area.label,
+       sex = sex.label,
+       age_group = classif1.label,
+       year = time,
+       rate = obs_value)
+
+#Selektieren relevanter Variablen
+data_labour_force <- select(data_labour_force,
+                            country, sex, age_group, year, rate)
