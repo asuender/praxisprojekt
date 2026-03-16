b1e5a5da550de31bb5ffa2573f1283c4f89a21ee
mirih-13 <miriamhaf@outlook.de>
2026-03-16 13:13:57 +0100

    Refine data cleaning function for first analysis.

--- code/labour_force_participation.R
+++ code/labour_force_participation.R
@@ -6,6 +6,7 @@ load_labour_force_with_educ_data <- function() {
   read.csv(here("data", "raw", "ilo_labour_force_participation_and_education.csv.gz"))
 }
 
+
 #Function to prepare raw data
 prepare_lfpr_data <- function(data) {
   data <- data %>% select(
@@ -23,28 +24,33 @@ prepare_lfpr_data <- function(data) {
   data
 }
 
+
 #Prepare intermediate data for first analysis - five countries in year 2024
 prepare_lfpr_total_2024_comp <- function(data) {
   relevant_countries <- c(
-    "Tanzania" = "Tanzania, United Republic of" ,
-    "Germany" = "Germany",
-    "USA" = "United States of America",
-    "Iran" = "Iran (Islamic Republic of)",
-    "Australia" = "Australia"
-    ) #Selection of countries
+    "Tanzania, United Republic of" ,
+    "Germany",
+    "United States of America",
+    "Iran (Islamic Republic of)",
+    "Australia"
+    ) #Select relevant countries
 
   data <- data %>% filter(country %in% relevant_countries, #Only selected countries
                           str_detect(education, "Total"), #No education levels considered
                           year == 2024 #Only data for 2024
-                          )
+                          ) %>%
+    mutate(country = ifelse(country %in% relevant_countries,
+                            c("Tanzania", "Germany", "USA", "Iran", "Australia"),
+                            country))
   data
 }
+
 
 #Function to prepare intermediate data for second analysis - Germany from 2010-2024
 prepare_lfpr_GER_2010_to_2024 <- function(data) {
   data <- data%>% filter(country == "Germany", #Only Germany
                          str_detect(education, "Total"), #No education levels considered
                          year >= 2010 #From year 2010 onwards
-                         )
+  )
   data
 }
