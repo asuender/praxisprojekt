826b076d266a045976d58b0e8bdf47f56900610a
mirih-13 <miriamhaf@outlook.de>
2026-03-18 16:02:41 +0100

    Adjusted functions and graph for question one and two. Added analysis of labour force by education level.

--- code/labour_force_participation.R
+++ code/labour_force_participation.R
@@ -33,18 +33,20 @@ prepare_lfpr_total_2024_comp <- function(data) {
     "United States of America",
     "Iran (Islamic Republic of)",
     "Australia"
-    ) #Select relevant countries
+  ) #Select relevant countries
 
   data <- data %>% filter(country %in% relevant_countries, #Only selected countries
                           str_detect(education, "Total"), #No education levels considered
                           year == 2024 #Only data for 2024
-                          ) %>%
-    mutate(country = ifelse(country %in% relevant_countries,
-                            c("Tanzania", "Germany", "USA", "Iran", "Australia"),
-                            country))
+  ) %>%
+    mutate(country = recode(country,
+                            "Tanzania, United Republic of" = "Tanzania" ,
+                            "Germany" = "Germany",
+                            "United States of America" = "USA",
+                            "Iran (Islamic Republic of)" = "Iran",
+                            "Australia" = "Australia"))
   data
 }
-
 
 #Function to prepare intermediate data for second analysis - Germany from 2010-2024
 prepare_lfpr_GER_2010_to_2024 <- function(data) {
@@ -52,5 +54,26 @@ prepare_lfpr_GER_2010_to_2024 <- function(data) {
                          str_detect(education, "Total"), #No education levels considered
                          year >= 2010 #From year 2010 onwards
   )
+  data
+}
+
+
+#Function to prepare intermediata data for third analysis - LFPR by education level
+prepare_lfpr_2024_by_education <- function(data) {
+  data <- data %>% filter(year == 2024,
+                          !str_detect(education, "Total|Not elsewhere"),
+                          when_any(country == "Germany",
+                                   country == "Iran (Islamic Republic of)",
+                                   country == "United States of America")) %>%
+    mutate(country = recode(country,
+                            "Germany" = "Germany",
+                            "United States of America" = "USA",
+                            "Iran (Islamic Republic of)" = "Iran")) %>%
+    mutate(education = str_remove(education, "^Education \\(ISCED-11\\):")) %>%
+    mutate(education = ifelse(
+      str_detect(education, "level"),
+      str_remove(education, " level"),
+      education))
+
   data
 }
